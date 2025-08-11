582e3b132b8bc2a669bb2f62eaf408c6506ccb5b
s870012 <s8700122000@gmail.com>
2025-08-11 16:59:26 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './asset/main.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-icons/font/bootstrap-icons'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 
